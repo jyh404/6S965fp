bb45516c12a6f56a69e6f5cf0e6cae2a9c8511af
Jonathan <jhuang25@mit.edu>
2025-12-07 23:50:28 -0500

    Basic framework for how to do control logic

---
+++ hdl-sim/prn_controller.sv
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+module prn_controller #
+	(
+		parameter integer C_S00_AXIS_TDATA_WIDTH	= 32,
+		parameter integer C_M00_AXIS_TDATA_WIDTH	= 32
+	)
+	(
+		// Ports of Axi Slave Bus Interface S00_AXIS
+		input wire  s00_axis_aclk, s00_axis_aresetn,
+		input wire  s00_axis_tlast, s00_axis_tvalid,
+		input wire [C_S00_AXIS_TDATA_WIDTH-1 : 0] s00_axis_tdata,
+		input wire [(C_S00_AXIS_TDATA_WIDTH/8)-1: 0] s00_axis_tstrb,
+		output logic  s00_axis_tready,
+
+		// Ports of Axi Master Bus Interface M00_AXIS
+		input wire  m00_axis_aclk, m00_axis_aresetn,
+		input wire  m00_axis_tready,
+		output logic  m00_axis_tvalid, m00_axis_tlast,
+		output logic [C_M00_AXIS_TDATA_WIDTH-1 : 0] m00_axis_tdata,
+		output logic [(C_M00_AXIS_TDATA_WIDTH/8)-1: 0] m00_axis_tstrb
+        
+	);
+
+
+  enum { EMPTY, BUSY, FULL } state;
+
+endmodule
+
+`default_nettype wire
