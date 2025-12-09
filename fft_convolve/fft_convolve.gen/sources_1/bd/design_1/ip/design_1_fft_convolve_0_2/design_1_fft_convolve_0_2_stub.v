4b6400f3bc9013aa0d91dde8378641d5f894591b
Jonathan <jhuang25@mit.edu>
2025-12-08 19:25:40 -0500

    minor fix: maybe 4096 only synthesized correctly this time?

--- fft_convolve/fft_convolve.gen/sources_1/bd/design_1/ip/design_1_fft_convolve_0_2/design_1_fft_convolve_0_2_stub.v
+++ fft_convolve/fft_convolve.gen/sources_1/bd/design_1/ip/design_1_fft_convolve_0_2/design_1_fft_convolve_0_2_stub.v
@@ -2,7 +2,7 @@
 // Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2025.1 (lin64) Build 6140274 Wed May 21 22:58:25 MDT 2025
-// Date        : Mon Dec  8 18:21:17 2025
+// Date        : Mon Dec  8 19:09:46 2025
 // Host        : eecs-digital-35 running 64-bit Ubuntu 24.04.3 LTS
 // Command     : write_verilog -force -mode synth_stub
 //               /home/jhuang25/Documents/6.S965/6S965fp/fft_convolve/fft_convolve.gen/sources_1/bd/design_1/ip/design_1_fft_convolve_0_2/design_1_fft_convolve_0_2_stub.v
