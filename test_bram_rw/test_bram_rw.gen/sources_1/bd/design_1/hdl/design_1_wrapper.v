66b9a8f8a1293205c7781d12a951f2b5a73a9e15
Jonathan <jhuang25@mit.edu>
2025-12-08 20:32:03 -0500

    two-port BRAM can be written to and from AXIS

---
+++ test_bram_rw/test_bram_rw.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -0,0 +1,21 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2025.1 (lin64) Build 6140274 Wed May 21 22:58:25 MDT 2025
+//Date        : Mon Dec  8 20:22:19 2025
+//Host        : eecs-digital-35 running 64-bit Ubuntu 24.04.3 LTS
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   (sw);
+  input [3:0]sw;
+
+  wire [3:0]sw;
+
+  design_1 design_1_i
+       (.sw(sw));
+endmodule
